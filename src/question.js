223588ca276eea5756daa1cd1526dad8b3a8dad8
Virginia Vanstavel <virginiavanstavel@Virginias-MacBook-Pro.local>
2026-03-27 20:38:55 +0100

    Update quiz and question files

--- src/question.js
+++ src/question.js
@@ -1,7 +1,19 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor (text, choices, answer, difficulty){
+       this.text = text
+       this.choices = choices
+       this.answer = answer
+       this.difficulty = difficulty
+    }
 
     // 2. shuffleChoices()
+    shuffleChoices(){
+    const number = Math.floor (Math.random()*this.choices.length)
+    return this.choices[number];
 }
+    
+}
+const question = new Question('Hello', ["rabbit" , "cat", "dog"])
+ console.log(question.shuffleChoices())
+ // figure out why we needed to add the new variable
+
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,40 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
+    constructor (questions, timeLimit, timeRemaining){
+    this.questions = questions
+    this.timeLimit = timeLimit
+    this.timeRemaining = timeRemaining
+    this.correctAnswers = 0
+    this.currentQuestionIndex = 0
+    }
+
+    
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+    }
     
     // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+        return this.questions[this.currentQuestionIndex];
+    }
 
     // 4. shuffleQuestions()
+    shuffleQuestions(){
+        let randomQuestion = Math.floor(Math.random()* this.questions.length)
+        return this.questions[randomQuestion];
+    }
 
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
 }
+
+const testQuestions = ["question1", "question2", "question3", "question4", "question5", "question6", "question7"];
+      // Instantiate a new Quiz object with the test questions
+      const quiz = new Quiz(testQuestions, 60, 60);
+      // Call the shuffleQuestions() method to shuffle the questions array in the quiz
+      console.log(quiz.shuffleQuestions());
+      
